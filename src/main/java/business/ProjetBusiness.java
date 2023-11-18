a62c8bfa1c6071f1e8befd8e169b62e014a04fde
bengagioussama <oussama.bengagi@esprit.tn>
2023-11-18 13:03:16 +0100

    first commit

---
+++ src/main/java/business/ProjetBusiness.java
@@ -0,0 +1,56 @@
+package business;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+
+import entity.Projet;
+
+public class ProjetBusiness {
+
+	 public static List<Projet> projets;
+	    ChefProjetBusiness ChPB=new ChefProjetBusiness();
+	    public ProjetBusiness() {
+	    	projets= new ArrayList<>();
+	    }
+	    
+	    
+	 
+	 public String addProjet(Projet p){
+	       
+	    		if( projets.add(p))
+	    		   return "Projet a été ajouté avec succès";
+	    		   else 
+	    			   return null ;
+	    }
+	 
+	 public List<Projet> getProjets() {
+	        return projets;
+	    }
+	
+	 
+	    public boolean deleteProjet(int id){
+	        Iterator<Projet> iterator=projets.iterator();
+	        while(iterator.hasNext()){
+	            Projet p=iterator.next();
+	            if(p.getId()==id){
+	                iterator.remove();
+	                return true;
+	            }
+	        }
+	        return false;
+	    }
+	 
+	 
+	    public Projet getProjetByMatricule(String matricule){
+
+	        for (Projet l:projets){
+	        	 if(l.getChefprojet().getMatricule()==matricule)
+	        	
+	                return l;
+	        }
+	        return null;
+	    }
+	    
+}
